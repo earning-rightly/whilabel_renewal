a17be371202d68238ab73ca8be0dc688cdae2340
Jong Min Seong <joungminseong@MacBook-Air-3.local>
2024-05-27 11:28:43 +0900

    <feature> Set up mypage development inital environment

---
+++ lib/screen/home/mock_home_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/** 개발할때 사용할 임시 Home view */
+class MockHomeView extends StatelessWidget {
+
+  const MockHomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
