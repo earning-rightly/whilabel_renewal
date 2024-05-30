320de8a1bcb486f88ebb10e6ba5fdb8ec90fdf2c
sangmin han <sangriel@naver.com>
2024-05-31 00:49:59 +0900

    add svg icon path

---
+++ lib/design_guide_managers/svg_icon_path.dart
@@ -0,0 +1,61 @@
+class SvgIconPath {
+  static String announce = "assets/icon/announce.svg";
+  static String faq = "assets/icon/f_a_q.svg";
+  static String customer = "assets/icon/customer.svg";
+  static String star = "assets/icon/star.svg";
+  // barcodeScan Page
+  static String barcode = "assets/icon/barcode.svg";
+  static String cameraShutter = "assets/icon/camera_shutter.svg";
+  static String image = "assets/icon/image.svg";
+
+  // login Page
+  static String instargramIcon = "assets/login/instagram_icon.svg";
+  static String kakaoIcon = "assets/login/kakao_icon.svg";
+  static String googleIcon = "assets/login/google_icon.svg";
+  static String appleIcon = "assets/login/apple_icon.svg";
+  static String  whilabelIcon = "assets/login/Whilabel.svg";
+
+  static String document = "assets/icon/document.svg";
+  static String setting = "assets/icon/settings.svg";
+  static String toggleOff = "assets/icon/toggle_off.svg";
+  static String toggleOn = "assets/icon/toggle_on.svg";
+  static String backBold = "assets/icon/_back/back_bold.svg";
+  static String close = "assets/icon/close.svg";
+  static String notification = "assets/icon/notification.svg";
+  static String notificationNew = "assets/icon/notification_new.svg";
+  static String whisky = "assets/icon/whisky.svg";
+  static String camera = "assets/icon/camera.svg";
+  static String user = "assets/icon/user.svg";
+
+  static String checkBold = "assets/icon/_check/check_Bold.svg";
+
+  static String downLoad = "assets/icon/download.svg";
+  static String list = "assets/icon/list.svg";
+  static String grid = "assets/icon/grid.svg";
+
+  static String menu = "assets/icon/menu.svg";
+  static String album = "assets/icon/album.svg";
+
+  static String onBoardingStep1 =
+      "assets/icon/on_boarding/on_boarding_step1.svg";
+  static String onBoardingStep2 =
+      "assets/icon/on_boarding/on_boarding_step2.svg";
+  // taste icon
+  static String tasteChocolat = "assets/icon/_taste/taste_chocolate.svg";
+  static String tasteCitric = "assets/icon/_taste/taste_citric.svg";
+  static String tasteDriedFruit = "assets/icon/_taste/taste_dried_fruit.svg";
+  static String tasteFreshFruit = "assets/icon/_taste/taste_fresh_fruit.svg";
+  static String tasteHony = "assets/icon/_taste/taste_honey.svg";
+  static String tasteHusky = "assets/icon/_taste/taste_husky.svg";
+  static String tasteMedicinal = "assets/icon/_taste/taste_medicinal.svg";
+  static String tasteNutty = "assets/icon/_taste/taste_nutty.svg";
+  static String tasteSherried = "assets/icon/_taste/taste_sherried.svg";
+  static String tasteSmokey = "assets/icon/_taste/taste_smokey.svg";
+  static String tasteTobacco = "assets/icon/_taste/taste_tobacco.svg";
+  static String tasteVanilla = "assets/icon/_taste/taste_vanilla.svg";
+
+  // pop menu
+  static String delete = "assets/icon/pop_menu/delete.svg";
+  static String modify = "assets/icon/pop_menu/modify.svg";
+  static String share = "assets/icon/pop_menu/share.svg";
+}
