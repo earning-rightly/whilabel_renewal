1a4a2ceeca4914d3b177cbd4a08a685baf03fd44
sangriel <39114237+sangriel@users.noreply.github.com>
2024-06-02 01:13:11 +0900

    Asset 파일 추가 (#7)
    * assetsfile추가
    
    * add image path
    
    * add svg icon path

---
+++ lib/design_guide_managers/image_path.dart
@@ -0,0 +1,4 @@
+const loginPngImage = "assets/login/login_background.png";
+const cameraViewPngImage = "assets/screen_image/camera_view_image.png";
+const placeHolderImage = "assets/screen_image/whisky_place_holder.png";
+const playstoreImage = "assets/logo/playstore.png";
