f65cd10ac5362ffbd89722e77cca32669c74ee4f
joungminseong <reputable4126@gmail.com>
2024-06-06 22:56:41 +0900

    sub widget UserCritqueContainer() 추가
    ArchivingPostDetailView()에서 사용되는 sub widget을 추가합니다

---
+++ lib/screen/whiskey_detail/sub_widget/user_crique_container/user_crique_container_view_model.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whilabel_renewal/data/taste/taste_feature.dart';
+
+import './user_crique_container_state.dart';
+
+final userCriqueContainerViewModelProvider = StateNotifierProvider<
+    UserCriqueContainerViewModel,
+    UserCriqueContainerState>((ref) => UserCriqueContainerViewModel(ref));
+
+class UserCriqueContainerViewModel
+    extends StateNotifier<UserCriqueContainerState> {
+  final Ref ref;
+
+  UserCriqueContainerViewModel(this.ref)
+      : super(UserCriqueContainerState.initial(
+            tasteFeatures: [], starScore: 3, note: ""));
+
+  void setState(
+      {required String note,
+      required double starScore,
+      required List<TasteFeature> features}) {
+    Map<String, int> tasteFeatureMap = {};
+
+    features.forEach((feature) {
+      tasteFeatureMap[feature.title] = feature.value;
+    });
+
+    state = state.copyWith(
+      featureMap: tasteFeatureMap,
+      tasteFeatures: features,
+      note: note,
+      starScore: starScore,
+    );
+  }
+
+  void onChangeStarScore(double score) {
+    state = state.copyWith(starScore: score);
+    print(score);
+  }
+
+  Future<void> onChangeTasteFeature(String featureName, int value) async {
+    var map = {...state.featureMap};
+
+    map[featureName] = value;
+    state = state.copyWith(featureMap: map);
+  }
+
+  Future<double> getStarScore() async {
+    return state.starScore;
+  }
+
+  Future<List<TasteFeature>> getFeatures() async {
+    final features = state.tasteFeatures;
+    final Map<String, int> featureMap = state.featureMap;
+
+    final List<TasteFeature> updatedFeatures = [];
+
+    for (var feature in features) {
+      final newFeature = feature.copyWith(
+          value: featureMap[feature.title] ??
+              feature.value // 저장하는데 이상이 있으면 원래 값으로 저장
+          );
+      updatedFeatures.add(newFeature);
+    }
+
+    return updatedFeatures;
+  }
+}
