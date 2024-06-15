d5d85759fd35ecb3c04d9085cbaf966ae43e5369
joungminseong <reputable4126@gmail.com>
2024-06-15 16:17:30 +0900

    detaillPost로 이동하는 버튼 추가

--- lib/screen/home/mock_home_view.dart
+++ lib/screen/home/mock_home_view.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whilabel_renewal/data/mock_data/archiving_post/mock_archiving_post.dart';
+import 'package:whilabel_renewal/screen/home/mock_home_view_model.dart';
 import 'package:whilabel_renewal/screen/login/login_view.dart';
-import 'package:whilabel_renewal/screen/login/login_view_model.dart';
 import 'package:whilabel_renewal/screen/my_page/setting_view/setting_view.dart';
 import 'package:whilabel_renewal/screen/onboarding/onboarding_view.dart';
 import 'package:whilabel_renewal/screen/resign/resign_view.dart';
 import 'package:whilabel_renewal/screen/util_views/loading_view/loading_view.dart';
 import 'package:whilabel_renewal/screen/util_views/loading_view/loading_view_model.dart';
 
+import 'sub_widget/mock_post.dart';
+
 /** 개발할때 사용할 임시 Home view */
 class MockHomeView extends ConsumerWidget {
-  const MockHomeView({Key? key}) : super(key: key);
+  MockHomeView({Key? key}) : super(key: key);
+
+  final _provider = MockHomeViewModel().provider;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final posts = ref.watch(_provider).posts;
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
@@ -24,11 +30,18 @@ class MockHomeView extends ConsumerWidget {
             child: Column(
               children: [
                 SizedBox(height: 30),
+                for (MockArchivingPost post in posts)
+                  ColoredBox(
+                    color: Colors.black,
+                    child: MockPost(archivingPost: post),
+                  ),
                 TextButton(
                     onPressed: () {
                       ref.read(loadingViewModelProvider.notifier).hideLoading();
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => SettingView()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SettingView()));
                     },
                     child: Text("setting view")),
                 SizedBox(height: 30),
@@ -42,19 +55,22 @@ class MockHomeView extends ConsumerWidget {
                 TextButton(
                     onPressed: () {
                       ref.read(loadingViewModelProvider.notifier).showLoading();
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => ResignView()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ResignView()));
                     },
                     child: Text("resignView")),
                 SizedBox(height: 30),
                 TextButton(
                     onPressed: () {
                       ref.read(loadingViewModelProvider.notifier).showLoading();
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => OnBoardingView()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => OnBoardingView()));
                     },
                     child: Text("onboarding")),
-
               ],
             ),
           ),
