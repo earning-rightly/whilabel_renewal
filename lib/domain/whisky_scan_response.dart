7fe2159b0b0e853aad65a058319d0945bc3c60b7
joungminseong <reputable4126@gmail.com>
2024-06-28 17:57:57 +0900

    domain/에 WhiskyScan과 WhiskyScanResponse 추가

---
+++ lib/domain/whisky_scan_response.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'base_response.dart';
+import 'whisky_scan.dart';
+
+part 'whisky_scan_response.g.dart';
+
+@JsonSerializable()
+class WhiskyScanResponse extends BaseResponses<WhiskyScan> {
+  WhiskyScanResponse({
+    required super.message,
+    required super.data,
+  });
+
+  factory WhiskyScanResponse.fromJson(Map<String, dynamic> json) =>
+      _$WhiskyScanResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WhiskyScanResponseToJson(this);
+}
