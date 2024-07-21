a763eabae9dd82f5b1306eb8f2bb28616eb89547
joungminseong <reputable4126@gmail.com>
2024-07-22 00:13:40 +0900

    이전  바코드 값 저장하는 로직 추가

--- lib/screen/scan_main_page/custom_barcode_scanner/custom_barcode_scanner_view.dart
+++ lib/screen/scan_main_page/custom_barcode_scanner/custom_barcode_scanner_view.dart
@@ -1,11 +1,12 @@
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:camera/camera.dart';
 import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 import 'package:whilabel_renewal/screen/scan_main_page/custom_barcode_scanner/sub_widget/detector_view.dart';
+
+String _barcodeNotFoundMessage = "바코드가 발견되지 않았습니다";
+int MAX_REPEATED_BARCODE_COUNT = 5;
 
 class CustomBarcodeScannerView extends ConsumerStatefulWidget {
   const CustomBarcodeScannerView({super.key});
@@ -14,18 +15,19 @@ class CustomBarcodeScannerView extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() {
     return _CustomBarcodeScannerViewState();
   }
-
 }
 
-class _CustomBarcodeScannerViewState extends ConsumerState<CustomBarcodeScannerView> {
-
+class _CustomBarcodeScannerViewState
+    extends ConsumerState<CustomBarcodeScannerView> {
   final BarcodeScanner _barcodeScanner = BarcodeScanner();
   bool _canProcess = true;
   bool _isBusy = false;
   CustomPaint? _customPaint;
   String? _text;
   var _cameraLensDirection = CameraLensDirection.back;
-  var _barcode = "바코드가 발견되지 않았습니다";
+  var _barcode = _barcodeNotFoundMessage;
+  int repeatedBarcodeCount = 0;
+  String preBarcode = "";
 
   @override
   void dispose() {
@@ -58,15 +60,29 @@ class _CustomBarcodeScannerViewState extends ConsumerState<CustomBarcodeScannerV
       _text = '';
     });
     final barcodes = await _barcodeScanner.processImage(inputImage);
+
     if (inputImage.metadata?.size != null &&
         inputImage.metadata?.rotation != null) {
-      if (barcodes.length != 0 ) {
-        _barcode = barcodes.last.displayValue ?? "바코드가 발견되지 않았습니다";
-      }
-      else {
-        _barcode = "바코드가 발견되지 않았습니다";
-      }
 
+      if (barcodes.length != 0 && barcodes.last.displayValue != null) {
+        String? lastBarcode = barcodes.last.displayValue;
+
+        if (lastBarcode != preBarcode &&
+            lastBarcode != _barcodeNotFoundMessage) {
+          _barcode = lastBarcode!;
+          preBarcode = lastBarcode;
+          repeatedBarcodeCount= 0;
+        }
+
+        if (lastBarcode == preBarcode) {
+          repeatedBarcodeCount++;
+        }
+      }
+      if (repeatedBarcodeCount >= MAX_REPEATED_BARCODE_COUNT) {
+        _barcode = preBarcode;
+      } else {
+        _barcode = _barcodeNotFoundMessage;
+      }
     } else {
       String text = 'Barcodes found: ${barcodes.length}\n\n';
       for (final barcode in barcodes) {
